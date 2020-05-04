64736e47894d1d9629db9be3eca19fc439727d21
rimeorange <rimeorange83@gmail.com>
2020-05-04 14:01:05 +0900

    Todo : 사이트 기본정보가 화면에 나타나게 하라

--- src/main/java/todoapp/web/todo/TodoController.java
+++ src/main/java/todoapp/web/todo/TodoController.java
@@ -1,16 +1,33 @@
 package todoapp.web.todo;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import todoapp.web.model.SiteProperties;
 
 @Controller
 public class TodoController {
 
-    @RequestMapping("/todos")
+    /*@RequestMapping("/todos")
     public ModelAndView todos() {
         ModelAndView mav = new ModelAndView("todos");
         return mav;
+    }*/
+
+    private Environment env;
+    private SiteProperties properties;
+
+    public TodoController(Environment env, SiteProperties properties) {
+        this.env = env;
+        this.properties = properties;
     }
 
+    @RequestMapping("/todos")
+    public  ModelAndView todos() {
+        ModelAndView mav = new ModelAndView("todos");
+        mav.addObject("site", properties);
+
+        return mav;
+    }
 }
