fa5e0eb9e7fc38a7caf7a8d83bb9cb98a30508ca
rimeorange <rimeorange83@gmail.com>
2020-05-31 22:14:40 +0900

    Todo : Csv 다운로드 기능 추가

--- src/main/java/todoapp/web/WebMvcConfiguration.java
+++ src/main/java/todoapp/web/WebMvcConfiguration.java
@@ -1,14 +1,22 @@
 package todoapp.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import todoapp.commons.web.view.CommaSeparatedValuesView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Spring Web MVC 설정
  *
  * @author springrunner.kr@gmail.com
  */
+@Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
@@ -20,10 +28,15 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     /**
      * 스프링부트가 생성한 ContentNegotiatingViewResolver를 조작할 목적으로 작성된 컴포넌트
      */
+    @Configuration
     public static class ContentNegotiationCustomizer {
 
+        @Autowired
         public void configurer(ContentNegotiatingViewResolver viewResolver) {
             // TODO ContentNegotiatingViewResolver 사용자 정의
+            List<View> views = new ArrayList<>(viewResolver.getDefaultViews());
+            views.add(new CommaSeparatedValuesView());
+            viewResolver.setDefaultViews(views);
         }
 
     }
--- src/main/java/todoapp/web/todo/TodoController.java
+++ src/main/java/todoapp/web/todo/TodoController.java
@@ -4,6 +4,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import todoapp.core.todos.application.TodoFinder;
+import todoapp.web.convert.TodoToSpreadsheetConverter;
 import todoapp.web.model.SiteProperties;
 
 @Controller
@@ -17,17 +19,19 @@ public class TodoController {
 
     private Environment env;
     private SiteProperties properties;
+    private TodoFinder todoFinder;
 
-    public TodoController(Environment env, SiteProperties properties) {
+    public TodoController(Environment env, SiteProperties properties, TodoFinder todoFinder) {
         this.env = env;
         this.properties = properties;
+        this.todoFinder = todoFinder;
     }
 
     @RequestMapping("/todos")
     public  ModelAndView todos() {
         ModelAndView mav = new ModelAndView("todos");
         mav.addObject("site", properties);
-
+        mav.addObject(new TodoToSpreadsheetConverter().convert(todoFinder.getAll()));
         return mav;
     }
 }
